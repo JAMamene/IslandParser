af2bf77f43407d52e6323127bd50cedb794c8b6a
JA Puskaric <jean-adam.puskaric@etu.unice.fr>
2017-05-17 23:16:45 +0200

    petit ajout qui fait plaiz encore une fois on se met bien mamène

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     var turns = $("turn");
     var context = $("context");
+
+    // create popup corresponding for each turn
     turns.click(function () {
         var index = $("turn").index(this) + 1;
         var content = $(this).contents();
@@ -12,24 +14,34 @@ $(document).ready(function () {
             $(this).remove();
         })
     });
-    context.append("<divider/>")
+
+    // create computed data to put after context
+    context.append("<divider/>");
     context.append("<block><bold>Total actions : </bold>" + turns.length + "</block>");
     context.append("<block><bold>Money spent : </bold>" + sum() + "</block>");
-
     var actions = ["echo", "scan", "heading", "fly", "glimpse", "explore", "scout", "move_to", "exploit", "transform"];
     for (var i in actions) {
         context.append("<" + actions[i] + "><bold>" + actions[i] + "</bold> : " + count(actions[i]) + "</" + actions[i] + ">");
     }
 });
 
+/**
+ * counts the occurrences of a specified action
+ *
+ * @param str the action to count
+ * @returns {jQuery} count of the specified action
+ */
 function count(str) {
     return $("action[type=" + str + "]").length;
 }
 
+/**
+ * Realize the sum of all costs
+ * @returns {number} the total cost
+ */
 function sum() {
     var total = 0;
     $("cost").each(function (d) {
-
         total += parseInt($(this).text());
     });
     return total;
